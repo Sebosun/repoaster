3d9263184c557b80e37591f2869443e3105a349a
Sebastian <sebos@protonmail.com>
2025-10-21 23:58:49 +0200

    feat: new logger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,29 @@
 import express from "express";
 import router from "@/router/router";
 import cors from "cors";
-import { logger } from "@/logger";
+import pino from "pino";
+
+export const logger = pino();
 
 const port = 3000;
-
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.options('*', cors())
-
+app.options("*", cors());
 
 // TODO: get some better logging
 app.use((req, _, next) => {
-    logger.info("New request received:", req.method, req.url, req.body);
-    next();
+  logger.info(
+    { method: req.method, url: req.url, body: req.body },
+    "New request received:",
+  );
+  next();
 });
 
 app.use("/", router);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
